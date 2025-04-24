2a13101bdaffaa2515eafda3e48756840d89e82b
nahueldev <nanode@DESKTOP-DE3Q1J6>
2025-04-24 16:54:56 -0300

    update item extra

--- 2025_AED_TP1_1K06_G023_Weigandt_418608_Perez_418190_Gerez-Peña_418034.py
+++ 2025_AED_TP1_1K06_G023_Weigandt_418608_Perez_418190_Gerez-Peña_418034.py
@@ -14,6 +14,22 @@ elif tipo_moneda[1] in moneda_pagar:
 elif tipo_moneda[2] in moneda_pagar:
     monto_base = round((93 * monto_nominal) / 100, 2)
 
+    if monto_base >= 0 and monto_base <= 10000:
+        monto_base = round((95 * monto_base) / 100, 2)
+        if monto_base < 100:
+            monto_base = round((93 * monto_nominal) / 100, 2)
+
+    if monto_base > 10000 and monto_base <= 20000:
+        monto_base = round((92.48 * monto_base) / 100, 2)
+
+    if monto_base > 20000 and monto_base <= 50000:
+        monto_base = round((91.78 * monto_base) / 100, 2)
+
+    if monto_base > 50000:
+        monto_base = round((84.6 * monto_base) / 100, 2)
+        if monto_base > 70000:
+            monto_base = round((97 * monto_base) / 100, 2)
+
 elif tipo_moneda[3] in moneda_pagar:
     monto_base = round((91 * monto_nominal) / 100, 2)
 
