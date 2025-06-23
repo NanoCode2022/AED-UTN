8c4e5744aa6a5b8a38bc5e338f25c9d8ee3d0f39
Nahuel <nanocode2022@gmail.com>
2025-06-23 19:35:04 -0300

    revisar calcula bien las comisiones pero no dan los mismos resultados en r4 y r10

--- tp2/tp2.py
+++ tp2/tp2.py
@@ -20,6 +20,13 @@ ordenes_eur = 0
 ordenes_gbp = 0
 ordenes_jpy = 0
 cantidad_primer_nombre = 0
+difenrencia = 0
+calculo_diferencial = 0
+
+r10 = ''
+r11 = r12 = r16 = 0
+
+sum_monto_final_ARS = cant_monto_ARS = 0
 
 
 for linea in o:
@@ -38,7 +45,7 @@ for linea in o:
     monedas_encontradas = [
         moneda for moneda in monedas_posibles if moneda in cod_orden_pago
     ]
-
+    print(monedas_encontradas)
     if len(monedas_encontradas) >= 2 or len(monedas_encontradas) == 0:
         moneda_inv += 1
         cont_op_inv += 1
@@ -68,9 +75,17 @@ for linea in o:
         cant_oper_validas += 1
         # r4
         suma_montos_finales += soporte.calculo_monto_final(int(id_alg_calc_mon_final), soporte.calculo_monto_base(int(id_alg_calc_com),int(monto_nominal)))
+        if monedas_encontradas[0] == 'ARS':
+            sum_monto_final_ARS += soporte.calculo_monto_final(int(id_alg_calc_mon_final), soporte.calculo_monto_base(int(id_alg_calc_com),int(monto_nominal)))
+            cant_monto_ARS += 1
 
-        print("paso montofinal")
-    
+    difenrencia = int(monto_nominal) - soporte.calculo_monto_final(int(id_alg_calc_mon_final), soporte.calculo_monto_base(int(id_alg_calc_com),int(monto_nominal)))
+
+    if difenrencia > calculo_diferencial:
+        calculo_diferencial = difenrencia 
+        r10 = cod_orden_pago
+        r11 = monto_nominal
+        r12 = soporte.calculo_monto_final(int(id_alg_calc_mon_final), soporte.calculo_monto_base(int(id_alg_calc_com),int(monto_nominal)))
     
     
    
@@ -86,6 +101,8 @@ for linea in o:
     moneda_valida = False
     dest_mal_ident = False
 
+
+r16 = sum_monto_final_ARS // cant_monto_ARS
 print(" (r1) - Cantidad de ordenes invalidas - moneda no autorizada:", moneda_inv)
 print(
     " (r2) - Cantidad de ordenes invalidas - beneficiario mal identificado:",
@@ -98,7 +115,10 @@ print(" (r6) - Cantidad de ordenes de pago en USD:", ordenes_usd)
 print(" (r7) - Cantidad de ordenes de pago en EUR:", ordenes_eur)
 print(" (r8) - Cantidad de ordenes de pago en GBP:", ordenes_gbp)
 print(" (r9) - Cantidad de ordenes de pago en JPY:", ordenes_jpy)
-
+print('(r10) - Codigo de la orden de pago con mayor diferencia nominal - final:', r10)
+print('(r11) - Monto nominal de esa misma orden:', r11)
+print('(r12) - Monto final de esa misma orden:', r12)
 print('(r13) - Nombre del primer beneficiario del archivo:', primer_nombre)
 print('(r14) - Cantidad de veces que apareció ese mismo nombre:', cantidad_primer_nombre)
 print('(r15) - Porcentaje de operaciones inválidas sobre el total:', ((cont_op_inv * 100) // contador))
+print('(r16) - Monto final promedio de las ordenes validas en moneda ARS:', r16)
