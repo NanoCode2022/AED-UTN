613c5c553684ed8abd1bbb073c0ffb8cda501188
Nahuel <nanocode2022@gmail.com>
2025-06-23 19:08:50 -0300

    falta r10 11 12 16

--- tp2/soporte.py
+++ tp2/soporte.py
@@ -1,11 +1,16 @@
 def identificador_destinatario(codigo):
     codigo = codigo.strip()
     tiene_dig_may_gui = True
-    digitos_validos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+    digitos_validos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-"
+    contador = 0
+    
     for c in codigo:
+        if c in "_-":
+           contador += 1 
         if c not in digitos_validos:
             tiene_dig_may_gui = False
-
+    if len(codigo) == contador:
+        tiene_dig_may_gui = False
     return tiene_dig_may_gui
 
 
@@ -14,3 +19,64 @@ def operaciones_validas(mon_inv, desti_mal_iden):
         return True
     else:
         return False
+
+
+def calculo_monto_base(algoritmo,monto_nominal):
+    comision = 0
+    print(algoritmo,'alg')
+
+    if algoritmo == 1:
+        print('pasears')
+        comision = monto_nominal * 0.09
+
+    if algoritmo == 2:
+        print('paseusd')
+        if monto_nominal < 50000:
+            comision = 0
+        elif monto_nominal < 80000:
+            comision = monto_nominal * 0.05
+        else:
+            comision = monto_nominal * 0.078
+
+    if algoritmo == 3:
+        print('paseeurgba')
+        comision = 100  # comisión fija
+        if monto_nominal > 25000:
+            comision += monto_nominal * 0.06
+
+    if algoritmo == 4:
+        print('pasejpy')
+        if monto_nominal <= 100000:
+            comision = 500  # comisión fija
+        if monto_nominal > 100000:
+            comision = 1000
+    if algoritmo == 5:
+        print('pasears2')
+        if monto_nominal < 500000:
+            comision = 0
+        if monto_nominal >= 500000:
+            comision += monto_nominal * 0.07
+        if comision > 50000:
+            comision = 50000
+    monto_base = monto_nominal - comision
+    return monto_base
+
+
+def calculo_monto_final(algoritmo,monto_base):
+    algoritmo = int(algoritmo)
+    impuesto = 0
+    if algoritmo == 1:
+        if monto_base <= 300000:
+            impuesto = 0
+        if monto_base > 300000:
+            excedente = monto_base - 300000
+            impuesto = excedente * 0.25
+    if algoritmo == 2:
+        if monto_base < 50000:
+            impuesto = 50
+        if monto_base >= 50000:
+            impuesto = 1000
+    if algoritmo == 3:
+        impuesto = monto_base * 0.03
+    monto_final = monto_base - impuesto
+    return monto_final
--- tp2/tp2.py
+++ tp2/tp2.py
@@ -1,53 +1,104 @@
 import soporte
-
 o = open("ordenes.txt", "rt")
+o.readline()
 
-monedas_posibles = ("ARS", "USD", "EUR", "GBP", "JYP")
+monedas_posibles = ("ARS", "USD", "EUR", "GBP", "JPY")
 
+contador = 0
+cont_op_inv = 0
+primer_nombre = None
+moneda_valida = False
 moneda_inv = 0
-destinatario_mal_ident = 0
+destinatario_mal_ident = False
+cont_dest_mal_iden = 0
 orden_incorrecta = 0
 cant_oper_validas = 0
+suma_montos_finales = 0
+ordenes_ars = 0
+ordenes_usd = 0
+ordenes_eur = 0
+ordenes_gbp = 0
+ordenes_jpy = 0
+cantidad_primer_nombre = 0
 
 
 for linea in o:
-    linea = o.readline()
     print(linea)
-
+    contador += 1
     nom_desti = linea[0:20]
     id_desti = linea[20:30]
     cod_orden_pago = linea[30:40]
     monto_nominal = linea[40:50]
     id_alg_calc_com = linea[50:52]
     id_alg_calc_mon_final = linea[52:54]
-
+    
     # validez de una orden de pago
 
     # r1_
-
     monedas_encontradas = [
         moneda for moneda in monedas_posibles if moneda in cod_orden_pago
     ]
 
-    if len(monedas_encontradas) >= 2:
+    if len(monedas_encontradas) >= 2 or len(monedas_encontradas) == 0:
         moneda_inv += 1
+        cont_op_inv += 1
+    else:
+
+        #r5,r6,r7,r8,r9
+        if monedas_encontradas[0] == "ARS":
+            ordenes_ars += 1
+        elif monedas_encontradas[0] == "USD":
+            ordenes_usd += 1
+        elif monedas_encontradas[0] == "EUR":
+            ordenes_eur += 1
+        elif monedas_encontradas[0] == "GBP":
+            ordenes_gbp += 1
+        elif monedas_encontradas[0] == "JPY":
+            ordenes_jpy += 1
+        moneda_valida = True
 
     # el codigo de indentificacion del destinatario debe contener letras mayusculas digitos guiones.
     # r2_
-    if not soporte.identificador_destinatario(id_desti):
-        destinatario_mal_ident += 1
+    if not soporte.identificador_destinatario(id_desti) and moneda_valida:
         dest_mal_ident = True
-    else:
-        dest_mal_ident = False
-
+        cont_dest_mal_iden += 1
+        cont_op_inv += 1
     # r3
-    if soporte.operaciones_validas(moneda_inv, dest_mal_ident):
+    if moneda_valida and not dest_mal_ident:
         cant_oper_validas += 1
-    # r4
+        # r4
+        suma_montos_finales += soporte.calculo_monto_final(int(id_alg_calc_mon_final), soporte.calculo_monto_base(int(id_alg_calc_com),int(monto_nominal)))
+
+        print("paso montofinal")
+    
+    
+    
+   
+    
+    #r13
+    if contador == 1:
+        primer_nombre = nom_desti.strip()
+    #r14
+    if primer_nombre in nom_desti:
+        cantidad_primer_nombre += 1
+
+    monedas_encontradas = []
+    moneda_valida = False
+    dest_mal_ident = False
 
 print(" (r1) - Cantidad de ordenes invalidas - moneda no autorizada:", moneda_inv)
 print(
     " (r2) - Cantidad de ordenes invalidas - beneficiario mal identificado:",
-    destinatario_mal_ident,
+    cont_dest_mal_iden,
 )
 print(" (r3) - Cantidad de operaciones validas:", cant_oper_validas)
+print(' (r4) - Suma de montos finales de operaciones validas:', round(suma_montos_finales, 2))
+print(" (r5) - Cantidad de ordenes de pago en ARS:", ordenes_ars)
+print(" (r6) - Cantidad de ordenes de pago en USD:", ordenes_usd)
+print(" (r7) - Cantidad de ordenes de pago en EUR:", ordenes_eur)
+print(" (r8) - Cantidad de ordenes de pago en GBP:", ordenes_gbp)
+print(" (r9) - Cantidad de ordenes de pago en JPY:", ordenes_jpy)
+
+print('(r13) - Nombre del primer beneficiario del archivo:', primer_nombre)
+print('(r14) - Cantidad de veces que apareció ese mismo nombre:', cantidad_primer_nombre)
+print('(r15) - Porcentaje de operaciones inválidas sobre el total:', ((cont_op_inv * 100) // contador))
